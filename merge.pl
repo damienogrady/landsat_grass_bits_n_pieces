804b11f4005df154767c85854262bb939acc4744
Damien O'Grady <damien.ogrady@my.jcu.edu.au>
2015-02-26 14:21:53 +1000

    Merge file completed

--- merge.pl
+++ merge.pl
@@ -4,8 +4,9 @@ use strict;
 use warnings;
 
 ######################################################################################
-# This is intended to take two arguments, the first being the base map, and the second
-# being a map which you wish to mosaic to the first.  It is assumed that the two maps
+# This is intended to take three arguments, the first being the base map, and the second
+# being a map which you wish to mosaic to the first.  The third argument is the name you
+# want to give the output map.                          It is assumed that the two maps
 # overlap.  Regression is carried out using the region in which the two maps overlap,
 # and then the resulting coefficients are used to apply a linear transformation to the
 # second map by way of a radiometric "correction", to try to "blend" the two maps
@@ -14,8 +15,8 @@ use warnings;
 # Damien O'Grady 26-Feb-2015
 # ####################################################################################
 #
-my ($base, $map) = @ARGV or die "You must argue a base map and another map to merge\n";
+my ($base, $map, $output) = @ARGV or die "You must argue a base map and another map to merge\n";
 
-my %univars = get_univars();
+my %coeffs = split /[=\n\r]/, `r.regression.line -g map1=$map map2=$base `;
 
-
+system "r.mapcalc '$output = if(isnull($map), $base, $coeff{b} * $map + $coeff{a})' ";
